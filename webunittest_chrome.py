beed3f2ec6a0920c4b1ab56ce183444ad5215300
qinjinjia <31771154+qinjinjia@users.noreply.github.com>
2017-11-26 23:38:50 -0500

    Update webunittest_chrome.py

--- webunittest_chrome.py
+++ webunittest_chrome.py
@@ -24,7 +24,7 @@ class BasicTest(unittest.TestCase):
 
         caps = {}
         
-        caps['name'] = 'Basic Test Example'
+        caps['name'] = 'Qinjin Jia's Resume'
         caps['build'] = '1.0'
         caps['browserName'] = 'Chrome'
         caps['version'] = '61x64'
